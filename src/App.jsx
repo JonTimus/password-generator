e6b813fc0ac99831ae14087003e260db0f7c4d12
JonTimus <timusjohn@gmail.com>
2023-10-12 00:36:22 -0500

    fixed colors & few UI changes

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,7 @@ function App() {
     window.navigator.clipboard.writeText(password)
     passwordRef.current?.select() // 
     setCopyButtonText('copied')
+    // console.log(password)
   }
 
   const resetCopyButtonText = () => {
@@ -43,21 +44,22 @@ function App() {
 
   const regeneratePassword = () => {
     generatePassword()
+    resetCopyButtonText()
   }
 
   const getPasswordDifficulty = () => {
     const criteriaCount = (numberAllowed ? 1 : 0) + (charAllowed ? 1 : 0) + (upperCaseAllowed ? 1 : 0) + (lowerCaseAllowed ? 1 : 0)
     if (criteriaCount === 1) {
       return (
-        <span style={{ color: 'red' }}>Easy</span>
+        <span className='text-red-600 text-sm'>Easy</span>
       )
     } else if (criteriaCount === 2) {
       return (
-        <span style={{ color: 'yellow' }}>Medium</span>
+        <span className='text-amber-400 text-sm'>Medium</span>
       )
     } else if (criteriaCount >= 3) {
       return (
-        <span style={{ color: 'green' }}>Hard</span>
+        <span className='text-lime-600 text-sm'>Hard</span>
       )
     } else {
       return ''
@@ -66,12 +68,13 @@ function App() {
 
   useEffect(() => {
     generatePassword()
-  }, [length, numberAllowed, charAllowed, upperCaseAllowed, lowerCaseAllowed])
+  }, [length, numberAllowed, charAllowed, upperCaseAllowed, lowerCaseAllowed, generatePassword])
 
   return (
-    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-white text-orange-500'>
-      <h1 className='text-white text-center my-3'>Password Generator</h1>
-      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-white text-neutral-800'>
+      <h1 className='text-neutral-800 text-center my-1'>PASSWORD GENERATOR</h1>
+      <p className='text-neutral-600 text-center my-5 text-sm'>Create a strong and secure passwords to keep your account safe online.</p>
+      <div className='flex shadow rounded-lg overflow-hidden'>
         <div className='relative flex-grow'>
           <input
             type='text'
@@ -84,70 +87,74 @@ function App() {
           <button onClick={regeneratePassword} className='absolute right-0 top-0 h-full bg-white px-2 py-0.5'>
             <FontAwesomeIcon icon={faRedo} />
           </button>
-          <button onClick={copyPasswordToClipboard} className='absolute right-10 top-0 h-full bg-cyan-600 text-white px-3 py-0.5'>{copyButtonText}</button>
+          <button onClick={copyPasswordToClipboard} className='absolute right-10 top-0 h-full bg-cyan-500 text-white px-3 py-0.5'>{copyButtonText}</button>
         </div>
       </div>
+      <div className='text-gray-400 text-left mb-4'>
+          {getPasswordDifficulty()}
+      </div>
       <div className='flex text-sm flex-col gap-y-2'>
+        <label className='text-sm' htmlFor='length'>Password Length: {length}</label>
         <div className='flex items-center gap-x-1'>
           <input
             type="range"
             min={6}
             max={100}
             value={length}
-            className='cursor-pointer flex-grow'
+            className='cursor-pointer flex-grow accent-cyan-500'
             onChange={(e) => {
               setLength(e.target.value)
               resetCopyButtonText()
             }}
           />
-          <label htmlFor='length'>Length: {length}</label>
         </div>
         <div className='flex items-center gap-x-1'>
-          <input
-            type="checkbox"
-            defaultChecked={numberAllowed}
-            onChange={() => {
-              setNumberAllowed((prev) => !prev)
-              resetCopyButtonText()
-            }}
-          />
-          <label htmlFor='number'>Numbers</label>
-        </div>
-        <div className='flex items-center gap-x-1'>
-          <input
-            type="checkbox"
-            defaultChecked={charAllowed}
-            onChange={() => {
-              setCharAllowed((prev) => !prev)
-              resetCopyButtonText()
-            }}
-          />
-          <label htmlFor='charInput'>Special Characters</label>
-        </div>
-        <div className='flex items-center gap-x-1'>
+          <label htmlFor='upperCase'>Uppercase</label>
           <input
             type="checkbox"
             defaultChecked={upperCaseAllowed}
+            className='w-5 h-5 bg-white accent-cyan-500 border border-gray-600 rounded-md cursor-pointer ml-auto'
             onChange={() => {
               setUpperCaseAllowed((prev) => !prev)
               resetCopyButtonText()
             }}
           />
-          <label htmlFor='upperCase'>Uppercase</label>
         </div>
         <div className='flex items-center gap-x-1'>
+          <label htmlFor='lowerCase'>Lowercase</label>
           <input
             type="checkbox"
             defaultChecked={lowerCaseAllowed}
+            className='w-5 h-5 bg-white accent-cyan-500 border border-gray-600 rounded-md cursor-pointer ml-auto'
             onChange={() => {
               setLowerCaseAllowed((prev) => !prev)
               resetCopyButtonText()
             }}
           />
-          <label htmlFor='lowerCase'>Lowercase</label>
         </div>
-        <div className='text-center text-gray-400'>
-          Password Difficulty: {getPasswordDifficulty()}
+        <div className='flex items-center gap-x-1'>
+          <label htmlFor='number'>Numbers</label>
+          <input
+            type="checkbox"
+            defaultChecked={numberAllowed}
+            className='w-5 h-5 bg-white accent-cyan-500 border border-gray-600 rounded-md cursor-pointer ml-auto'
+            onChange={() => {
+              setNumberAllowed((prev) => !prev)
+              resetCopyButtonText()
+            }}
+          />
+        </div>
+        <div className='flex items-center gap-x-1'>
+          <label htmlFor='charInput'>Special Characters</label>
+          <input
+            type="checkbox"
+            defaultChecked={charAllowed}
+            className='w-5 h-5 bg-white accent-cyan-500 border border-gray-600 rounded-lg cursor-pointer ml-auto'
+            onChange={() => {
+              setCharAllowed((prev) => !prev)
+              resetCopyButtonText()
+            }}
+          />
         </div>
       </div>
     </div>
